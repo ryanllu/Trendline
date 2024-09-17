5744ddddeff41b8b855f3b377b2f10cbf383d7a5
Cyiaostch <tejasatya1612@gmail.com>
2024-09-17 14:10:48 +0700

    README update

--- README.md
+++ README.md
@@ -10,15 +10,36 @@ You can install the `trendline` library using pip:
 pip install trendline
 ```
 
-## Usage
+## Example Use
 
-### Importing the Library
-
-To use the library, first import the necessary functions:
+### Fetching Example Data
 
 ```python
-from trendline import calculate_trendlines_multiple, calculate_trendline_single
+import pandas as pd
+import yfinance as yf
+
+# Set the start and end dates
+start_date = "2024-01-01"
+end_date = "2024-09-01"
+
+# Fetch Bitcoin data from Yahoo Finance
+bitcoin = yf.download('BTC-USD', start=start_date, end=end_date)
+
+# Ensure that the index is a DatetimeIndex
+bitcoin.index = pd.to_datetime(bitcoin.index)
+
+# Resample the data to a 2-day frequency and aggregate OHLC values
+bitcoin = bitcoin.resample('3D').agg({
+    'Open': 'first',
+    'High': 'max',
+    'Low': 'min',
+    'Close': 'last'
+})[-60:]
 ```
+
+Initial price chart:
+
+![Line Plot](images/line.svg)
 
 
 ### Calculating Trendlines for a Single Asset
@@ -41,34 +62,59 @@ A dictionary containing trendline data for the single asset, including:
 
 Example
 ```python
-import pandas as pd
 from trendline import calculate_trendline
-import yfinance as yf
-from datetime import datetime
-
-# Set the start and end dates
-start_date = "2024-01-01"
-end_date = "2024-09-01"
-
-# Fetch S&P 500 data from Yahoo Finance
-sp500 = yf.download('^GSPC', start=start_date, end=end_date)
-sp500 = sp500.resample('3D').ffill() 
 
 # Calculate support and resistance line
-sr = calculate_trendline(sp500)
+trend_line = calculate_trendline(bitcoin)
 
-print(sr)
+print(trend_line)
 ```
 
 Output
 ```python
-{'resistance_line_gradient': 11.467201234683744,
- 'support_line_gradient': 6.881977271455867,
- 'resistance_line_start': 4924.116809777279,
- 'resistance_line_end': 5841.492908551978,
- 'support_line_start': 4688.572767152106,
- 'support_line_end': 5239.130948868576}
+{
+  'support_first_value': 61290.158653846156, 
+  'support_last_value': 49481.25901442308, 
+  'support_gradient': -200.1508413461538, 
+  'resistance_first_value': 71895.0775082237, 
+  'resistance_last_value': 68705.86307565791, 
+  'resistance_gradient': -54.054481907894726}
 ```
+
+### Visualize Trendline
+You can then visualize the trendline using data visualization library like `trendline`.
+
+```python
+  from trendline import calculate_trendline
+  from fsplot import plot_candlestick_trendline
+
+  # Calculate trendline
+  trend_line = calculate_trendline(bitcoin)
+
+
+  # Prepare data for plotting
+  data = {
+      "Open": bitcoin["Open"],
+      "High": bitcoin["High"],
+      "Low": bitcoin["Low"],
+      "Close": bitcoin["Close"],
+      "support_first_value": trend_line["support_first_value"],
+      "support_gradient": trend_line["support_gradient"],
+      "resistance_first_value": trend_line["resistance_first_value"],
+      "resistance_gradient": trend_line["resistance_gradient"]
+  }
+
+  # Generate and display the candlestick plot
+  svg = plot_candlestick_trendline(data)
+
+  with open("candlestick_trendline.svg", "w") as file:
+    file.write(svg)
+```
+
+Output:
+
+![Candlestick Plot with Trendline](images/candlestick_trendline.svg)
+
 
 
 ### Calculating Trendlines for Multiple Assets
@@ -93,52 +139,50 @@ A dictionary where keys are asset identifiers and values are dictionaries contai
 Example
 ```python
 import pandas as pd
-from trendline import calculate_multiple_trendlines
 import yfinance as yf
-from datetime import datetime
+from trendline import calculate_multiple_trendlines
 
 # Set the start and end dates
 start_date = "2024-01-01"
 end_date = "2024-09-01"
 
-# Fetch S&P 500 data from Yahoo Finance
-sp500 = yf.download('^GSPC', start=start_date, end=end_date)
-sp500 = sp500.resample('3D').ffill() 
+# Fetch Bitcoin data from Yahoo Finance
+bitcoin = yf.download('BTC-USD', start=start_date, end=end_date)
 
-# Fetch Nikkei 225 data from Yahoo Finance
-n225 = yf.download('^N225', start=start_date, end=end_date)
-n225 = n225.resample('3D').ffill() 
+# Fetch Bitcoin data from Yahoo Finance
+ethereum = yf.download('ETH-USD', start=start_date, end=end_date)
 
-data={"nikkei225":n225,
-      "s&p500":sp500}
+data={
+  "bitcoin":bitcoin,
+  "ethereum":ethereum
+}
 
-# Calculate multiple support and resistance lines
-srs = calculate_multiple_trendlines(data)
+multiple_trendlines = calculate_multiple_trendlines(data)
 
-print(srs)
 ```
 
 Output
 ```python
 {
-'nikkei225':
-    {
-      'resistance_line_gradient': 39.69566114193926,
-      'support_line_gradient': 21.805160216854723,
-      'resistance_line_start': 39599.66548566491,
-      'resistance_line_end': 42735.62271587811,
-      'support_line_start': 33908.11786810943,
-      'support_line_end': 35630.72552524095
-    },
- 's&p500':
-    {
-      'resistance_line_gradient': 11.467201234683744,
-      'support_line_gradient': 6.881977271455867,
-      'resistance_line_start': 4924.116809777279,
-      'resistance_line_end': 5841.492908551978,
-      'support_line_start': 4688.572767152106,
-      'support_line_end': 5239.130948868576
-    }
+  'bitcoin': 
+  {
+    'support_first_value': 41460.49592550204,
+    'support_last_value': 59401.33995907945,
+    'support_gradient': 73.83063388303462,
+    'resistance_first_value': 74427.21580630442,
+    'resistance_last_value': 65303.645883028614,
+    'resistance_gradient': -37.545555239818135
+  },
+ 
+ 'ethereum': 
+ {
+    'support_first_value': 2335.8713278169453,
+    'support_last_value': 2552.775168729424,
+    'support_gradient': 0.8926083988167839,
+    'resistance_first_value': 4267.282737989803,
+    'resistance_last_value': 3473.1311736899124,
+    'resistance_gradient': -3.2681134333328807
+  }
 }
 ```
 
--- build/lib/trendline/trendline.py
+++ build/lib/trendline/trendline.py
@@ -2,7 +2,97 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-def calculate_trendlines_multiple(dataframes, num_chunks=3, residual_percentile=5):
+def calculate_trendline(dataframe, num_chunks=3, residual_percentile=5):
+    """
+    Calculate trendlines for a single asset.
+
+    Parameters:
+    - dataframe (pd.DataFrame): A DataFrame containing financial data for the asset.
+    - num_chunks (int, optional): Number of chunks to divide residuals into (default is 5).
+    - residual_percentile (int, optional): Percentile for calculating residual cutoffs (default is 10).
+
+    Returns:
+    - dict: Dictionary containing trendline data:
+      - 'support_first_value': First value of the support trendline.
+      - 'support_last_value': Last value of the support trendline.
+      - 'support_gradient': Gradient of the support trendline.
+      - 'resistance_first_value': First value of the resistance trendline.
+      - 'resistance_last_value': Last value of the resistance trendline.
+      - 'resistance_gradient': Gradient of the resistance trendline.
+    """
+    # Select the data for the asset
+    asset_data = dataframe
+    index_array = np.arange(len(asset_data))
+    
+    # Process the support (low) prices
+    support_prices = asset_data['Low']
+    X_support = index_array.reshape(-1, 1)
+
+    # Perform linear regression on the support prices
+    support_model = LinearRegression()
+    support_model.fit(X_support, support_prices)
+    support_predictions = support_model.predict(X_support)
+    support_residuals = support_prices - support_predictions
+
+    total_residuals = len(support_residuals)
+    residuals_per_chunk = total_residuals // num_chunks
+
+    support_residual_indices_list = []
+    resistance_residual_indices_list = []
+
+    # Process each chunk of residuals
+    for chunk_index in range(num_chunks):
+        start_idx = chunk_index * residuals_per_chunk
+        end_idx = start_idx + residuals_per_chunk if chunk_index < num_chunks - 1 else total_residuals
+        residuals_in_chunk = support_residuals.iloc[start_idx:end_idx]
+
+        support_residual_cutoff = np.percentile(residuals_in_chunk, residual_percentile)
+        resistance_residual_cutoff = np.percentile(residuals_in_chunk, 100 - residual_percentile)
+
+        support_residual_indices = residuals_in_chunk[residuals_in_chunk <= support_residual_cutoff].index
+        resistance_residual_indices = residuals_in_chunk[residuals_in_chunk >= resistance_residual_cutoff].index
+
+        support_residual_indices_list.append(support_residual_indices)
+        resistance_residual_indices_list.append(resistance_residual_indices)
+
+
+    # Process the support trendline
+    combined_support_residual_indices = pd.Index(np.concatenate(support_residual_indices_list))
+    combined_resistance_residual_indices = pd.Index(np.concatenate(resistance_residual_indices_list))
+
+    support_indices_selected = [asset_data.index.get_loc(idx) for idx in combined_support_residual_indices]
+    support_selected_prices = asset_data['Low'].iloc[support_indices_selected]
+    X_selected_support = np.array(support_indices_selected).reshape(-1, 1)
+    support_trend_model = LinearRegression()
+    support_trend_model.fit(X_selected_support, support_selected_prices)
+    support_line_gradient = support_trend_model.coef_[0]
+
+    support_line_start = support_trend_model.predict([[index_array[0]]])[0]
+    support_line_end = support_trend_model.predict([[index_array[-1]]])[0]
+
+    # Process the resistance trendline
+    resistance_indices_selected = [asset_data.index.get_loc(idx) for idx in combined_resistance_residual_indices]
+    resistance_selected_prices = asset_data['High'].iloc[resistance_indices_selected]
+    X_selected_resistance = np.array(resistance_indices_selected).reshape(-1, 1)
+    resistance_trend_model = LinearRegression()
+    resistance_trend_model.fit(X_selected_resistance, resistance_selected_prices)
+    resistance_line_gradient = resistance_trend_model.coef_[0]
+
+    resistance_line_start = resistance_trend_model.predict([[index_array[0]]])[0]
+    resistance_line_end = resistance_trend_model.predict([[index_array[-1]]])[0]
+
+    # Return the trendline data
+    return {
+        'support_first_value': support_line_start,
+        'support_last_value': support_line_end,
+        'support_gradient': support_line_gradient,
+        'resistance_first_value': resistance_line_start,
+        'resistance_last_value': resistance_line_end,
+        'resistance_gradient': resistance_line_gradient
+    }
+
+
+def calculate_multiple_trendlines(dataframes, num_chunks=3, residual_percentile=5):
     """
     Calculate trendlines for multiple assets.
 
@@ -13,12 +103,12 @@ def calculate_trendlines_multiple(dataframes, num_chunks=3, residual_percentile=
 
     Returns:
     - dict: Dictionary where keys are asset identifiers and values are dictionaries containing trendline data:
-      - 'resistance_line_gradient': Gradient of the resistance (high) trendline.
-      - 'support_line_gradient': Gradient of the support (low) trendline.
-      - 'resistance_line_start': First value of the resistance (high) trendline.
-      - 'resistance_line_end': Last value of the resistance (high) trendline.
-      - 'support_line_start': First value of the support (low) trendline.
-      - 'support_line_end': Last value of the support (low) trendline.
+      - 'support_gradient': Gradient of the support trendline.
+      - 'support_first_value': First value of the support trendline.
+      - 'support_last_value': Last value of the support trendline.
+      - 'resistance_gradient': Gradient of the resistance trendline.
+      - 'resistance_first_value': First value of the resistance trendline.
+      - 'resistance_last_value': Last value of the resistance trendline.
     """
     trendlines_data = {}
     
@@ -84,102 +174,14 @@ def calculate_trendlines_multiple(dataframes, num_chunks=3, residual_percentile=
 
         # Store the results
         trendlines_data[asset_id] = {
-            'resistance_line_gradient': resistance_line_gradient,
-            'support_line_gradient': support_line_gradient,
-            'resistance_line_start': resistance_line_start,
-            'resistance_line_end': resistance_line_end,
-            'support_line_start': support_line_start,
-            'support_line_end': support_line_end,
+            'support_first_value': support_line_start,
+            'support_last_value': support_line_end,
+            'support_gradient': support_line_gradient,
+            'resistance_first_value': resistance_line_start,
+            'resistance_last_value': resistance_line_end,
+            'resistance_gradient': resistance_line_gradient
         }
     
     return trendlines_data
 
 
-def calculate_trendline_single(dataframe, num_chunks=3, residual_percentile=5):
-    """
-    Calculate trendlines for a single asset.
-
-    Parameters:
-    - dataframe (pd.DataFrame): A DataFrame containing financial data for the asset.
-    - num_chunks (int, optional): Number of chunks to divide residuals into (default is 5).
-    - residual_percentile (int, optional): Percentile for calculating residual cutoffs (default is 10).
-
-    Returns:
-    - dict: Dictionary containing trendline data:
-      - 'resistance_line_gradient': Gradient of the resistance (high) trendline.
-      - 'support_line_gradient': Gradient of the support (low) trendline.
-      - 'resistance_line_start': First value of the resistance (high) trendline.
-      - 'resistance_line_end': Last value of the resistance (high) trendline.
-      - 'support_line_start': First value of the support (low) trendline.
-      - 'support_line_end': Last value of the support (low) trendline.
-    """
-    # Select the data for the asset
-    asset_data = dataframe
-    index_array = np.arange(len(asset_data))
-    
-    # Process the support (low) prices
-    support_prices = asset_data['Low']
-    X_support = index_array.reshape(-1, 1)
-
-    # Perform linear regression on the support prices
-    support_model = LinearRegression()
-    support_model.fit(X_support, support_prices)
-    support_predictions = support_model.predict(X_support)
-    support_residuals = support_prices - support_predictions
-
-    total_residuals = len(support_residuals)
-    residuals_per_chunk = total_residuals // num_chunks
-
-    support_residual_indices_list = []
-    resistance_residual_indices_list = []
-
-    # Process each chunk of residuals
-    for chunk_index in range(num_chunks):
-        start_idx = chunk_index * residuals_per_chunk
-        end_idx = start_idx + residuals_per_chunk if chunk_index < num_chunks - 1 else total_residuals
-        residuals_in_chunk = support_residuals.iloc[start_idx:end_idx]
-
-        support_residual_cutoff = np.percentile(residuals_in_chunk, residual_percentile)
-        resistance_residual_cutoff = np.percentile(residuals_in_chunk, 100 - residual_percentile)
-
-        support_residual_indices = residuals_in_chunk[residuals_in_chunk <= support_residual_cutoff].index
-        resistance_residual_indices = residuals_in_chunk[residuals_in_chunk >= resistance_residual_cutoff].index
-
-        support_residual_indices_list.append(support_residual_indices)
-        resistance_residual_indices_list.append(resistance_residual_indices)
-
-
-    # Process the support trendline
-    combined_support_residual_indices = pd.Index(np.concatenate(support_residual_indices_list))
-    combined_resistance_residual_indices = pd.Index(np.concatenate(resistance_residual_indices_list))
-
-    support_indices_selected = [asset_data.index.get_loc(idx) for idx in combined_support_residual_indices]
-    support_selected_prices = asset_data['Low'].iloc[support_indices_selected]
-    X_selected_support = np.array(support_indices_selected).reshape(-1, 1)
-    support_trend_model = LinearRegression()
-    support_trend_model.fit(X_selected_support, support_selected_prices)
-    support_line_gradient = support_trend_model.coef_[0]
-
-    support_line_start = support_trend_model.predict([[index_array[0]]])[0]
-    support_line_end = support_trend_model.predict([[index_array[-1]]])[0]
-
-    # Process the resistance trendline
-    resistance_indices_selected = [asset_data.index.get_loc(idx) for idx in combined_resistance_residual_indices]
-    resistance_selected_prices = asset_data['High'].iloc[resistance_indices_selected]
-    X_selected_resistance = np.array(resistance_indices_selected).reshape(-1, 1)
-    resistance_trend_model = LinearRegression()
-    resistance_trend_model.fit(X_selected_resistance, resistance_selected_prices)
-    resistance_line_gradient = resistance_trend_model.coef_[0]
-
-    resistance_line_start = resistance_trend_model.predict([[index_array[0]]])[0]
-    resistance_line_end = resistance_trend_model.predict([[index_array[-1]]])[0]
-
-    # Return the trendline data
-    return {
-        'resistance_line_gradient': resistance_line_gradient,
-        'support_line_gradient': support_line_gradient,
-        'resistance_line_start': resistance_line_start,
-        'resistance_line_end': resistance_line_end,
-        'support_line_start': support_line_start,
-        'support_line_end': support_line_end,
-    }
